5a996e7a76a955fd4a7388a0c9a914fdb5166c45
baudoin <baudoinvicbolingo@gmail.com>
2025-02-21 17:15:37 +0200

    change the route

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/api", userRoutes);
 
 // Start the server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
