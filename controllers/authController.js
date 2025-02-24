ef2fe24d76d27f467c517cceabdf1e3e11660e52
baudoin <baudoinvicbolingo@gmail.com>
2025-02-24 13:40:45 +0200

    debuging my code

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,10 +30,10 @@ const registerUser = async (req, res) => {
 
 // User login
 const loginUser = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const passwordMatch = await bcrypt.compare(password, user.password);
