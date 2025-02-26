468d67744521be0b07bec94bfc09e6ebb3735866
baudoin <baudoinvicbolingo@gmail.com>
2025-02-26 16:22:01 +0200

    added a role

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
