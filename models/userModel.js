507a030162bcc93fffd9e9f2fdefe250a0e090ff
baudoin <baudoinvicbolingo@gmail.com>
2025-02-26 16:26:45 +0200

    debuging role for users

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,15 @@
+// const mongoose = require("mongoose");
+
+// const UserSchema = new mongoose.Schema({
+//   firstname: { type: String, required: true },
+//   lastname: { type: String, required: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+// });
+
+// module.exports = mongoose.model("User", UserSchema);
+
+
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
@@ -5,7 +17,11 @@ const UserSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["admin", "user", "student", "coordinator", "company"], 
+    default: "user",
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
