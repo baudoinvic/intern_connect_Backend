ab3254b742d02b2b06149e0e7064b5a4ffd9cf28
Baudoin <baudoinvicbolingo@gmail.com>
2025-06-13 09:11:43 +0200

    updated the admin dashboard

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getUsers,
-//   getUserById,
-//   createUser,
-//   updateUser,
-//   deleteUser,
-// } = require("../controllers/userController");
 
-// router.get("/users", getUsers);
-// router.get("/users/:id", getUserById);
-// router.post("/users", createUser);
-// router.put("/users/:id", updateUser);
-// router.delete("/users/:id", deleteUser);
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();
